ad765f7213a23a5d53e7426d333430f1e22fb6de
Vikas <tondev23@gmail.com>
2022-11-10 20:32:22 +0530

    Entry point

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from 'body-parser';
+import cors from "cors";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: "*"
+}));
+app.use(bodyParser.json());
+
+
+app.listen(PORT, () => console.log(`Server is runnning on port : http://localhost:${PORT}`));
